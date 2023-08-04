9f57b11662ed753835758a77d3e0d44ffb930542
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-08-04 15:38:01 +0300

    Updates

--- assets/ChannelPage-8d11911a.js
+++
@@ -1,1 +0,0 @@
-import{o as e,j as n}from"./index-8610a16a.js";const r=()=>{const{channelId:a}=e();return n.jsxs("div",{children:["Channel Page id--",a]})};export{r as default};
---
+++ assets/ChannelPage-fdbc6f8f.js
@@ -0,0 +1,1 @@
+import{o as e,j as n}from"./index-c9d9438f.js";const r=()=>{const{channelId:a}=e();return n.jsxs("div",{children:["Channel Page id--",a]})};export{r as default};
