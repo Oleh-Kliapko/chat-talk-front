af094d482406ad82c1ccab39c0372d82e18bdbbf
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-08-24 14:48:44 +0300

    Updates

--- assets/NotFoundPage-d3f6038e.js
+++ assets/NotFoundPage-d3f6038e.js
@@ -1,4 +1,4 @@
-import{n as e,q as n,j as t}from"./index-e914745c.js";const o="/chat-talk-front/assets/404-page-animation-d03ade57.gif",i=e.div`
+import{n as e,q as n,j as t}from"./index-d29feacc.js";const o="/chat-talk-front/assets/404-page-animation-d03ade57.gif",i=e.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
