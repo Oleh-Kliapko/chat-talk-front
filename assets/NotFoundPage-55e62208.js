10795e7099440741b72df1ff9bbeb244146bd20f
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-08-27 11:12:50 +0300

    Updates

--- assets/NotFoundPage-55e62208.js
+++ assets/NotFoundPage-55e62208.js
@@ -1,4 +1,4 @@
-import{n as e,q as n,j as t}from"./index-61668c27.js";const o="/chat-talk-front/assets/404-page-animation-d03ade57.gif",i=e.div`
+import{n as e,D as n,j as t}from"./index-73e73230.js";const o="/chat-talk-front/assets/404-page-animation-d03ade57.gif",i=e.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
