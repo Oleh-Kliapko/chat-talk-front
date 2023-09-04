b2f669d70661adb646d157a01be2e9a674d86623
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-09-04 11:56:15 +0300

    Updates

---
+++ assets/CreateChannel-0901e6e2.js
@@ -0,0 +1,79 @@
+import{n as r,t,r as n,u as T,a as A,Q as d,b as E,j as e,d as I,C as P}from"./index-664758b6.js";import{a as H}from"./index.esm-ddd366d6.js";import{P as U}from"./Pencil-b2c0260c.js";import{B as D}from"./BtnTemplate-9227dbcc.js";import{H as M}from"./Header-3b18fe14.js";import"./iconBase-a5f9828d.js";import"./index.esm-8a708275.js";const N=r.h4`
+font-size:${t.fontSizes.m};
+font-weight: 400;
+font-family:"Segoe-Regular";
+color:${t.colors.grey}
+`,V=r.div`
+width: ${t.breakpoints.mobile}px;
+display:flex;
+flex-direction:column;
+margin-top:8px;
+`,O=r.div`
+width: 100%;
+height:200px;
+margin-top:24px;
+margin-bottom:24px;
+position: relative;
+background-color:#EFEFEF;
+`,W=r.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`,q=r.img`
+width:100%;
+height:100%;
+object-fit: cover;
+
+`,G=r.button`
+position: absolute;
+top:12px;
+right:12px;
+color:"white"
+
+`,Q=r.input`
+width:100%;
+padding-top:12px;
+padding-bottom:12px;
+margin-top:8px;
+margin-bottom:16px;
+border:none;
+border-bottom:1px solid ${t.colors.grey};
+font-family:'Segoe-Regular';
+font-size:${t.fontSizes.m};
+font-weight:400;
+color:${t.colors.main};
+background-color:${t.colors.mainBgr};
+&:focus{
+    outline:none;
+}
+`,J=r.textarea`
+  width: 100%;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  margin-top: 8px;
+  resize: none;
+  border: none;
+  border-bottom: 1px solid ${t.colors.grey};
+  font-family: 'Segoe-Regular';
+  font-size: ${t.fontSizes.m};
+  font-weight: 400;
+  color: ${t.colors.main};
+  overflow: hidden;
+  background-color: ${t.colors.mainBgr};
+  line-height: 16px;
+  &:focus {
+    outline: none;
+  }
+`,j=r.label`
+font-family:'Segoe-SemiBold';
+font-size: ${t.fontSizes.l};
+font-weight: 600;
+color: ${t.colors.main};
+`,K=r.p`
+margin-left:auto;
+font-family:'Segoe-Regular';
+font-size:${t.fontSizes.m};
+font-weight:400;
+color:${t.colors.grey};
+`,X={value:"",rows:1,maxRows:5,maxLength:120,lineHeight:40},Y=()=>{const[s,p]=n.useState(!1),u=T(),[c,y]=n.useState(null),[m,f]=n.useState(null),[g,C]=n.useState(""),h=n.useRef(null),[a,S]=n.useState(X),x=A();n.useEffect(()=>{if(!c)return f(void 0);const o=URL.createObjectURL(c);return f(o),()=>URL.revokeObjectURL(o)},[c]);const v=n.useCallback(o=>{const{maxLength:i,maxRows:k,lineHeight:z}=a,l=o.target;l.style.height="auto";const R=l.scrollHeight,B=Math.ceil(R/z),L=Math.min(B,k);l.value.length>i&&(l.value=l.value.substring(0,i)),S(F=>({...F,value:l.value,rows:L})),l.scrollTop=0},[a]),b=n.useCallback(()=>{h.current.click()},[]),w=n.useCallback(o=>{if(o.target.files[0]){if(o.target.files[0].size>16e13)return d.warn("file is too large");if(o.target.files[0].type==="image/jpeg"||o.target.files[0].type==="image/png"){y(o.target.files[0]);return}return d.warn("wrong file type")}},[]),$=n.useCallback(async()=>{const o=new FormData;if(c&&o.append("image",c),o.append("title",g),o.append("description",a.value),!a.value||!g)return d.warn("fill all the fields");p(!0);const i=await u(E(o));console.log("response",i),i.meta.requestStatus==="fulfilled"?(d.success("channel created"),p(!1),x(`/channels/${i.payload.id}`)):(p(!1),d.error(i.error.message))},[g,u,x,c,a.value]);return e.jsxs(V,{children:[e.jsxs(e.Fragment,{children:["  ",e.jsx(N,{children:"Create your own channel from below"}),e.jsx(O,{children:m?e.jsxs(e.Fragment,{children:[e.jsx(q,{src:m,alt:"channel-image"}),!s&&e.jsxs(e.Fragment,{children:["  ",e.jsx(G,{onClick:b,type:"button",children:e.jsx(U,{})}),e.jsx("input",{type:"file",ref:h,onChange:w,style:{display:"none"}})]})]}):e.jsxs(W,{children:[e.jsx("button",{type:"button",children:e.jsx(H,{onClick:b,size:100,color:"#909090"})}),!s&&e.jsx("input",{type:"file",ref:h,onChange:w,style:{display:"none"}})]})}),e.jsxs(j,{children:["Channel Name",e.jsx(Q,{disabled:s,type:"text",name:"name",value:g,onChange:o=>C(o.target.value),placeholder:"Channel name"})]}),e.jsxs(j,{children:["Description",e.jsx(J,{disabled:s,name:"description",rows:a.rows,value:a.value,onChange:v,maxLength:a.maxLength,placeholder:"Write here..."})]}),e.jsxs(K,{children:[a.value.length,"/",a.maxLength]}),e.jsx(D,{disabled:s,onClick:$,text:s?"Wait...":"Create",textSize:t.fontSizes.m,color:t.colors.white,width:"100%",height:`${t.spacing[12]}px`,gradient:t.colors.mainBtnBgr,hoverGradient:t.colors.accentBtnBgr,border:t.border.main,borderRadius:t.radii.main,marginTop:"150px",marginBottom:"auto",type:"button"})]}),s&&e.jsx(I,{})]})},re=()=>e.jsxs(P,{children:[e.jsx(M,{title:"Create channel",goBack:!0}),e.jsx(Y,{})]});export{re as default};
