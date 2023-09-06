e8c91060ef0f9fab0f32066d4b4332817b6e6b51
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-09-06 08:57:18 +0300

    Updates

---
+++ assets/Header-b5dc0b46.js
@@ -0,0 +1,46 @@
+import{j as e,n,t as i,y as j,P as t,b as v}from"./index-1c0e3898.js";import{G as r}from"./iconBase-bed8070c.js";import{a as w}from"./index.esm-54b87603.js";const y=()=>e.jsxs("svg",{width:"40",height:"40",viewBox:"0 0 40 40",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[e.jsx("rect",{width:"40",height:"40",rx:"4",fill:"white"}),e.jsx("path",{d:"M28.4406 18.6056H21.0644V11.2294C21.0644 10.9033 20.9349 10.5906 20.7043 10.3601C20.4737 10.1295 20.1611 10 19.835 10C19.5089 10 19.1963 10.1295 18.9657 10.3601C18.7351 10.5906 18.6056 10.9033 18.6056 11.2294V18.6056H11.2294C10.9033 18.6056 10.5906 18.7351 10.3601 18.9657C10.1295 19.1963 10 19.5089 10 19.835C10 20.1611 10.1295 20.4737 10.3601 20.7043C10.5906 20.9349 10.9033 21.0644 11.2294 21.0644H18.6056V28.4406C18.6056 28.7667 18.7351 29.0794 18.9657 29.3099C19.1963 29.5405 19.5089 29.67 19.835 29.67C20.1611 29.67 20.4737 29.5405 20.7043 29.3099C20.9349 29.0794 21.0644 28.7667 21.0644 28.4406V21.0644H28.4406C28.7667 21.0644 29.0794 20.9349 29.3099 20.7043C29.5405 20.4737 29.67 20.1611 29.67 19.835C29.67 19.5089 29.5405 19.1963 29.3099 18.9657C29.0794 18.7351 28.7667 18.6056 28.4406 18.6056Z",fill:"url(#paint0_linear_276_1224)"}),e.jsx("defs",{children:e.jsxs("linearGradient",{id:"paint0_linear_276_1224",x1:"10",y1:"19.835",x2:"29.67",y2:"19.835",gradientUnits:"userSpaceOnUse",children:[e.jsx("stop",{stopColor:"#3459FE"}),e.jsx("stop",{offset:"1",stopColor:"#6216CF"})]})})]});function k(o){return r({tag:"svg",attr:{viewBox:"0 0 15 15",fill:"none"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M0.877014 7.49988C0.877014 3.84219 3.84216 0.877045 7.49985 0.877045C11.1575 0.877045 14.1227 3.84219 14.1227 7.49988C14.1227 11.1575 11.1575 14.1227 7.49985 14.1227C3.84216 14.1227 0.877014 11.1575 0.877014 7.49988ZM7.49985 1.82704C4.36683 1.82704 1.82701 4.36686 1.82701 7.49988C1.82701 8.97196 2.38774 10.3131 3.30727 11.3213C4.19074 9.94119 5.73818 9.02499 7.50023 9.02499C9.26206 9.02499 10.8093 9.94097 11.6929 11.3208C12.6121 10.3127 13.1727 8.97172 13.1727 7.49988C13.1727 4.36686 10.6328 1.82704 7.49985 1.82704ZM10.9818 11.9787C10.2839 10.7795 8.9857 9.97499 7.50023 9.97499C6.01458 9.97499 4.71624 10.7797 4.01845 11.9791C4.97952 12.7272 6.18765 13.1727 7.49985 13.1727C8.81227 13.1727 10.0206 12.727 10.9818 11.9787ZM5.14999 6.50487C5.14999 5.207 6.20212 4.15487 7.49999 4.15487C8.79786 4.15487 9.84999 5.207 9.84999 6.50487C9.84999 7.80274 8.79786 8.85487 7.49999 8.85487C6.20212 8.85487 5.14999 7.80274 5.14999 6.50487ZM7.49999 5.10487C6.72679 5.10487 6.09999 5.73167 6.09999 6.50487C6.09999 7.27807 6.72679 7.90487 7.49999 7.90487C8.27319 7.90487 8.89999 7.27807 8.89999 6.50487C8.89999 5.73167 8.27319 5.10487 7.49999 5.10487Z",fill:"currentColor"}}]})(o)}function b(o){return r({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z"}}]})(o)}function V(o){return r({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M304 336v40a40 40 0 01-40 40H104a40 40 0 01-40-40V136a40 40 0 0140-40h152c22.09 0 48 17.91 48 40v40m64 160l80-80-80-80m-192 80h256"}}]})(o)}const z=n.div`
+width: ${i.breakpoints.mobile}px;
+display:flex;
+justify-content:space-between;
+align-items:center;
+margin-top:50px;
+`,B=n.div`
+display:flex;
+align-items:flex-end;
+`,M=n.div`
+
+`,d=n.button`
+margin-right:16px;
+`,C=n.h1`
+font-family:"Segoe-Bold";
+font-size:${i.fontSizes.xl};
+font-weight:700;
+line-height:1,4;
+color:${i.colors.main};
+`,x=n(j)`
+color:${i.colors.grey}
+`,S=n.div`
+width:40px;
+height:40px;
+border-radius:50%;
+overflow:hidden;
+&:hover {
+  cursor: pointer;
+}
+`,$=n.img`
+width:100%;
+height:100%;
+`,H=n.h4`
+font-family:"Segoe-Bold";
+font-size:${i.fontSizes.m};
+font-weight:700;
+color:${i.colors.main};
+`,I=n.button`
+font-family:"Segoe-Regular";
+font-size:${i.fontSizes.m};
+font-weight:400;
+color:${i.colors.accent};
+&:hover{
+  cursor: pointer;
+}
+`,L=({title:o,goBack:g,close:f,profileLink:p,addChannelLink:m,imageUrl:l,channelName:a,id:u="123",done:c,subTitle:h})=>{const s=v();return e.jsxs(z,{children:[e.jsxs(B,{children:[g&&e.jsx(d,{onClick:()=>s(-1),children:e.jsx(w,{size:24})}),f&&e.jsx(d,{onClick:()=>s("/"),children:e.jsx(b,{size:24})}),o&&e.jsx(C,{children:o})]}),h&&e.jsx(C,{children:h}),a&&e.jsx(H,{children:a}),e.jsxs(M,{children:[c&&e.jsx(I,{type:"submit",onClick:c,children:"Done"}),l?e.jsx(S,{onClick:()=>s(`/about-channel/${u}`),children:e.jsx($,{src:l,width:40,height:40})}):e.jsxs(e.Fragment,{children:[m&&e.jsx(x,{to:"/create-channel",children:e.jsx(y,{size:20})}),p&&e.jsx(x,{to:"/my-profile",children:e.jsx(k,{size:40})})]})]})]})};L.propTypes={title:t.string,profileLink:t.bool,addChannelLink:t.bool,goBack:t.bool,close:t.bool,channelName:t.string,imageUrl:t.string,id:t.number,done:t.func,subTitle:t.string};export{L as H,V as I};
---
+++ assets/LoginPage-458d0889.js
@@ -0,0 +1,42 @@
+import{n,t as o,P as p,u as S,r as a,j as e,s as C,a as $,C as y,L as b}from"./index-1c0e3898.js";import{E as v,a as w,T as E,M,b as T,S as k,c as R,B as z,A as L}from"./SingingUpModal.styled-cf7db51a.js";import{C as x}from"./CreateModal-15412226.js";import{F as B,a as F,A as P}from"./AuthBtn-f742a54e.js";import{v as A}from"./index.esm-482c3647.js";import"./iconBase-bed8070c.js";import"./BtnTemplate-90a852b8.js";import"./ForgotPasswordBtn-b7a993be.js";import"./index.esm-468454a0.js";const O=n.div`
+ min-width: 390px;
+ padding:40px 20px;
+ `,D=n.h4`
+font-family:'Segoe-Bold';
+font-weight:700;
+font-size:${o.fontSizes.xl};
+color:${o.colors.main};
+`,I=n.p`
+margin-top:8px;
+font-family:'Segoe-Regular';
+font-weight:400;
+font-size:${o.fontSizes.m};
+color:${o.colors.main};
+`,V=n.div`
+margin-top:8px;
+  position: relative;
+`,W=n.input`
+  width: 100%;
+  color: ${o.colors.main};
+  font-family: 'Segoe-Regular';
+  font-size: ${o.fontSizes.m};
+  padding: ${o.spacing[3]}px ${o.spacing[0]}px ${o.spacing[3]}px
+    ${o.spacing[10]}px;
+  border: ${o.border.main};
+  border-radius: ${o.radii.main};
+
+  &:focus,
+  &:hover {
+    outline: none;
+    border: ${o.border.accent};
+    box-shadow: 0 0 0 1px ${o.colors.accent};
+  }
+`,q=n.div`
+  margin-top:32px;
+`,G=n.p`
+  color: ${o.colors.error};
+  font-size: ${o.fontSizes.xxs};
+  font-family: 'Segoe-Regular';
+  margin-bottom: ${o.spacing[4]}px;
+  align-self: flex-end;
+`,h=({onClose:t,onOpenRecieved:d})=>{const m=S(),[i,r]=a.useState(!1);return e.jsxs(O,{children:[e.jsx(D,{children:"Forgot password"}),e.jsx(I,{children:"Enter your email address, and we'll send you a link to reset your password"}),e.jsx(B,{validationSchema:A.emailSchema,initialValues:{email:""},onSubmit:async({email:l},{setSubmitting:s})=>{r(!0);const c=await m(C({email:l}));r(!1),c.meta.requestStatus!=="rejected"&&(t(),d(),s(!1))},children:({values:l,errors:s,touched:c,handleChange:f,handleBlur:g,handleSubmit:j})=>e.jsxs(F,{noValidate:!0,onSubmit:j,children:[e.jsx(q,{children:"Email"}),e.jsxs(V,{style:{position:"relative"},children:[e.jsx(v,{}),e.jsx(W,{type:"text",name:"email",onChange:f,onBlur:g,value:l.email,placeholder:"Email address"})]}),e.jsx(G,{children:s.email&&c.email&&s.email}),e.jsx(P,{disabled:i,from:"emailModal"})]})}),i&&e.jsx($,{})]})};h.propTypes={onClose:p.func,onOpenRecieved:p.func};const u=({onClose:t})=>e.jsxs(e.Fragment,{children:[e.jsxs(w,{children:[" ",e.jsx(E,{color:"white",size:54})," "]}),e.jsxs(M,{children:[e.jsx(T,{children:"Email received!"}),e.jsxs(k,{children:["Check your inbox for the password ",e.jsx("br",{}),"reset instructions"]}),e.jsx(R,{children:"Didn’t received the email yet?"}),e.jsx(z,{onClick:t,children:"Click here to try again"})]})]});u.propTypes={onClose:p.func};const _=()=>{const[t,d]=a.useState(!1),[m,i]=a.useState(!1),r=a.useCallback(()=>i(!1),[]),l=a.useCallback(()=>i(!0),[]),s=a.useCallback(()=>d(!1),[]),c=a.useCallback(()=>d(!0),[]);return e.jsxs(y,{children:[e.jsx(b,{widthLogo:"80",heightLogo:"80",title:"Login to Talk"}),e.jsx(L,{from:"loginPage",onOpen:l}),m&&e.jsx(x,{onClose:r,children:e.jsx(h,{onClose:r,onOpenRecieved:c})}),t&&e.jsx(x,{onClose:s,children:e.jsx(u,{onClose:s})})]})};export{_ as default};
