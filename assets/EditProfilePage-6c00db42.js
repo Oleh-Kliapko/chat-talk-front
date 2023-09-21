9e4b544e968ce3ac40d15b4bb41289c61281753a
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-09-21 21:46:31 +0300

    Updates

---
+++ assets/EditProfilePage-6c00db42.js
@@ -0,0 +1,54 @@
+import{n as a,t,P as u,r as o,Q as g,j as e,b as k,e as v,u as B,k as U,l as D,m as R,C as $}from"./index-71bb3e9b.js";import{B as z}from"./index.esm-fbad105a.js";import{B as P}from"./BtnTemplate-711465d7.js";import{H as L}from"./Header-6123a985.js";import{C as O}from"./CreateModal-90aad6bc.js";import{S as A}from"./SmallModal-889cc3a1.js";import"./iconBase-8dc62924.js";import"./index.esm-a64ee0f2.js";const E=a.div`
+width: ${t.breakpoints.mobile}px;
+display:flex;
+flex-direction:column;
+align-items:center;
+margin-top:12px;
+`,I=a.div`
+width:36px;
+height:36px;
+border-radius:50%;
+border: 1px solid #D3D3D3;
+display:flex;
+align-items:center;
+justify-content:center;
+position: absolute;
+bottom: 0;
+right: 0;
+background-color:${t.colors.white}
+`,M=a.div`
+position: relative;
+`,N=a.div`
+width:100px;
+height:100px;
+border-radius:50%;
+overflow:hidden;
+`,T=a.img`
+width:100%;
+height:100%;
+object-fit: cover;
+`,F=a.input`
+width:100%;
+padding-top:12px;
+padding-bottom:12px;
+margin-top:8px;
+border:none;
+border-bottom:1px solid ${t.colors.grey};
+font-family:'Segoe-Regular';
+font-size:${t.fontSizes.m};
+font-weight:400;
+color:${t.colors.main};
+background-color:${t.colors.mainBgr};
+&:focus{
+    outline:none;
+}
+`,H=a.label`
+
+font-family:'Segoe-SemiBold';
+font-size: ${t.fontSizes.l};
+font-weight: 600;
+color: ${t.colors.main};
+`,G=a.div`
+width:100%;
+margin-top:24px;
+`,j=({preview:l,userName:p,setSelectedPhoto:c,username:n,setUserName:h,onOpen:i})=>{const m=o.useRef(null),x=o.useCallback(()=>{m.current.click()},[]),f=o.useCallback(s=>{if(s.target.files[0]){if(s.target.files[0].size>16e13)return g.warn("file is too large");if(s.target.files[0].type==="image/jpeg"||s.target.files[0].type==="image/png"){c(s.target.files[0]);return}return g.warn("wrong file type")}},[c]);return e.jsxs(E,{children:[e.jsxs(M,{children:[e.jsx(N,{children:e.jsx(T,{src:l,alt:"channel-image"})}),e.jsxs(I,{children:[e.jsx(z,{onClick:x,size:24}),e.jsx("input",{type:"file",ref:m,onChange:f,style:{display:"none"}})]})]}),e.jsx(G,{children:e.jsxs(H,{children:["Username",e.jsx(F,{type:"text",name:"name",value:p,onChange:s=>h(s.target.value),placeholder:n})]})}),e.jsx(P,{onClick:i,text:"Delete Account",textSize:t.fontSizes.m,color:t.colors.main,width:"100%",height:`${t.spacing[12]}px`,hoverGradient:t.colors.accentBtnBgr,border:t.border.main,borderRadius:t.radii.main,marginTop:"415px",marginBottom:"auto",type:"button"})]})};j.propTypes={setSelectedPhoto:u.func,setUserName:u.func,onOpen:u.func,preview:u.string,userName:u.string,username:u.string};const Y=()=>{const l=k(),{avatarURL:p,username:c}=v(r=>r.auth.user),[n,h]=o.useState(null),[i,m]=o.useState(""),[x,f]=o.useState(p),[s,b]=o.useState(!1),w=o.useCallback(()=>{b(!0)},[]),y=o.useCallback(()=>{b(!1)},[]),d=B(),C=o.useCallback(async()=>{d(U()),d(D()),g.success("Account was successfully deleted"),l("/")},[d,l]);o.useEffect(()=>{if(n===null)return;const r=URL.createObjectURL(n);return f(r),()=>URL.revokeObjectURL(r)},[p,n,f]);const S=o.useCallback(async()=>{const r=new FormData;return n!==null&&r.append("profile_photo",n),i===""?r.append("username",c):r.append("username",i),i===""&&n===null?g.warn("nothing no change"):(d(R(r)),l("/my-profile"))},[d,l,n,i,c]);return e.jsxs($,{children:[e.jsx(L,{subTitle:"Account",goBack:!0,done:S}),e.jsx(j,{preview:x,setSelectedPhoto:h,username:c,setUserName:m,userName:i,onOpen:w}),s&&e.jsx(O,{onClose:y,children:e.jsx(A,{title:"Delete Account?",subtitle:"Are you sure want to delete your account?",yes:C,no:y})})]})};export{Y as default};
---
+++ assets/MainPage-796273ce.js
@@ -0,0 +1,82 @@
+import{n as a,t as e,P as g,u as L,e as $,r as t,z as M,j as s,A as w,B as y,D as A,E as I,C as R,a as V}from"./index-71bb3e9b.js";import{d as D,C as G}from"./ChanelList-e578b004.js";import{H}from"./Header-6123a985.js";import{G as N}from"./iconBase-8dc62924.js";import"./index.esm-a64ee0f2.js";function T(r){return N({tag:"svg",attr:{version:"1.1",id:"search",x:"0px",y:"0px",viewBox:"0 0 24 24",style:"enable-background:new 0 0 24 24;"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{d:`M20.031,20.79c0.46,0.46,1.17-0.25,0.71-0.7l-3.75-3.76c1.27-1.41,2.04-3.27,2.04-5.31
+		c0-4.39-3.57-7.96-7.96-7.96s-7.96,3.57-7.96,7.96c0,4.39,3.57,7.96,7.96,7.96c1.98,0,3.81-0.73,5.21-1.94L20.031,20.79z
+		 M4.11,11.02c0-3.84,3.13-6.96,6.96-6.96c3.84,0,6.96,3.12,6.96,6.96c0,3.84-3.12,6.96-6.96,6.96C7.24,17.98,4.11,14.86,4.11,11.02
+		z`}}]}]})(r)}const O=a.div`
+  width: ${e.breakpoints.mobile}px;
+  display:flex;
+  align-items:center;
+  margin-top:8px;
+`,q=a.div`
+position:relative;
+  flex: 1;
+`,J=a.div`
+position:absolute;
+top:8px;
+left:8px;
+color:${e.colors.grey};
+`;a.div`
+position:absolute;
+top:11px;
+right:11px;
+color:#4C4C4C;
+&:hover{
+  cursor: pointer;
+}
+`;a.div`
+position:relative;
+`;const K=a.input`
+width:100%;
+height:40px;
+padding-left:32px;
+padding-right:32px;
+font-size:${e.fontSizes.m};
+font-weight: 400;
+font-family:"Segoe-Regular";
+border: ${e.border.main};
+border-radius: ${e.radii.main};
+`;a.div`
+width:100px;
+position:absolute;
+border-radius:${e.radii.main};
+box-shadow:${e.shadows.main};
+background-color:${e.colors.white};
+padding:10px;
+display:flex;
+flex-direction:column;
+gap:16px; 
+bottom:-150%;
+right:0;
+z-index:2;
+`;a.div`
+font-size:${e.fontSizes.m};
+font-family:"Inter-Regular";
+&:hover{
+ color:${e.colors.accent};
+ font-weight:600;
+ cursor: pointer;
+}
+`;a.ul`
+width:100%;
+position:absolute;
+border-radius:${e.radii.main};
+box-shadow:${e.shadows.main};
+background-color:${e.colors.white};
+z-index:1;
+`;a.li`
+position: relative;
+padding:8px;
+height:40px;
+&:hover{
+  cursor: pointer;
+}
+`;a.div`
+position:absolute;
+color:${e.colors.grey};
+top:8px;
+right:8px;
+&:hover{
+  cursor: pointer;
+}
+`;a.button`
+margin:0;
+`;const j=({handleOnChange:r,setSearchList:c,setSearchValue:h})=>{const l=L(),u=$(i=>i.filter);t.useEffect(()=>{u!==""&&(c(!0),h(u))},[]);const d=t.useCallback(i=>{r(i),l(M(i.target.value))},[l,r]);return s.jsx(O,{children:s.jsxs(q,{children:[s.jsx(J,{children:s.jsx(T,{size:24})}),s.jsx(K,{autoFocus:!0,value:u,onChange:d,type:"text",placeholder:"Search"})]})})};j.propTypes={handleOnChange:g.func,setSearchValue:g.func,filter:g.string,setSearchList:g.func};const Z=()=>{const{channels:r,count:c,searchListChannels:h}=$(n=>n.channels),[l,u]=t.useState(1),[d,i]=t.useState(1),[p,f]=t.useState(!1),[x,S]=t.useState(""),[k,m]=t.useState(!1),[z,C]=t.useState(!1),o=L(),P=D(n=>{if(n.target.value.trim()===x)return;const b=n.target.value.trim().toLowerCase();if(S(b),b==="")return u(1),o(w()),f(!1);if(b!=="")return i(1),o(y()),f(!0)},1e3),v=t.useCallback(async()=>{p?(d===1?m(!0):C(!0),await o(A({page:d,search:x}))):p||(l===1?m(!0):C(!0),await o(I(l))),m(!1),C(!1)},[o,l,p,d,x]);t.useEffect(()=>()=>{o(w()),o(y())},[o]),t.useEffect(()=>{v()},[v]);const E=t.useCallback(()=>{h.length!==c&&i(n=>n+1)},[c,h]),F=t.useCallback(()=>{r.length!==c&&u(n=>n+1)},[r,c]);let B=p?h:r;return s.jsxs(R,{children:[s.jsx(H,{title:"Channels",goBack:!1,profileLink:!0,addChannelLink:!0}),s.jsx(j,{setSearchList:f,setSearchValue:S,handleOnChange:P}),k?s.jsx(V,{}):s.jsx(G,{from:"maim-page",channels:B,isLoading:z,ForwardPage:F,ForwardSearchPage:E,notFound:p})]})};export{Z as default};
