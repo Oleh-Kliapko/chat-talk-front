0836f93d1bb38a494e2c049e9b99d706b7875cc8
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-09-29 16:25:20 +0300

    Updates

---
+++ assets/ResetPasswordPage-d93e3b60.js
@@ -0,0 +1,19 @@
+import{n as o,t,j as e,C as n,L as r}from"./index-4f2593b1.js";import{B as s}from"./BtnTemplate-f8314f5e.js";import{M as i}from"./RecoverPasswordPage.styled-b9fa7425.js";const a=o.p`
+margin-top:44px;
+margin-bottom:32px;
+font-size:${t.fontSizes.m};
+font-weight:400;
+font-family: 'Segoe-Regular';
+`,g=o.p`
+margin-top:44px;
+font-size:${t.fontSizes.m};
+font-weight:400;
+font-family: 'Segoe-Regular';
+`,m=o.div`
+margin-top:32px;
+text-align:center;
+`,c=o.p`
+font-size:${t.fontSizes.m};
+font-weight:400;
+font-family: 'Segoe-Regular';
+`,d=()=>e.jsx(n,{children:e.jsxs(i,{children:[e.jsx(r,{widthLogo:"80",heightLogo:"80",title:"Welcome to Talk!"}),e.jsxs(a,{children:["Hi,",e.jsx("br",{}),"Forgot password?",e.jsx("br",{}),"We received a request to reset the password for your account."]}),e.jsx(g,{children:"To reset your password, click on the button below:"}),e.jsx(s,{text:"Reset password",textSize:t.fontSizes.m,color:t.colors.white,width:"100%",height:`${t.spacing[12]}px`,gradient:t.colors.mainBtnBgr,hoverGradient:t.colors.accentBtnBgr,border:t.border.main,borderRadius:t.radii.main,marginBottom:`${t.spacing[6]}px`,marginTop:`${t.spacing[8]}px`,type:"button"}),e.jsx(m,{children:e.jsxs(c,{children:["Thanks,",e.jsx("br",{}),"The Talk Team"]})})]})});export{d as default};
